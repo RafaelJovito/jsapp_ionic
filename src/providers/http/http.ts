64a5b8839f4ecba8df017ef4937bd3b22b61ec0d
Rafael Jovito <rafacj7@gmail.com>
2019-04-03 17:58:58 -0300

    [UPD] Provider Http, Readme

--- src/providers/http/http.ts
+++ src/providers/http/http.ts
@@ -1,17 +1,17 @@
+import { AlertProvider } from './../alert/alert';
+import { SpinnerProvider } from './../spinner/spinner';
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 
-/*
-  Generated class for the HttpProvider provider.
-
-  See https://angular.io/guide/dependency-injection for more info on providers
-  and Angular DI.
-*/
 @Injectable()
 export class HttpProvider {
 
-  constructor(public http: HttpClient) {
-    console.log('Hello HttpProvider Provider');
+  constructor(
+    private http: HttpClient,
+    private spinnerSrv: SpinnerProvider,
+    private alertSrv: AlertProvider
+    ) {
+
   }
 
 }
