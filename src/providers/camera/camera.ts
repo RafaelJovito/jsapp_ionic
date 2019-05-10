c764cc50ff982e72e8e9488b5feaac878c428440
Rafael Jovito <rafacj7@gmail.com>
2019-05-10 18:58:17 -0300

    [UPD] Objects Camera

--- src/providers/camera/camera.ts
+++ src/providers/camera/camera.ts
@@ -1,15 +1,59 @@
+import { Camera, CameraOptions } from '@ionic-native/camera/ngx';
 import { Platform } from 'ionic-angular';
-import { Injectable } from '@angular/core';
-import { Camera, CameraOptions} from '@ionic-native/camera';
+import { Injectable, ErrorHandler } from '@angular/core';
+
 
 @Injectable()
 export class CameraProvider {
 
   constructor(
-    private camera: CameraOptions,
-    private plataforma: Platform
-    ) {
+    private camera: Camera,
+    private platform: Platform) {
    
+  }
+
+  private _getPicture(source: number, callback): void{
+    if (this.platform.is('cordova')){
+
+      this.platform.ready().then(() => {
+        try {
+          let options: CameraOptions = {
+            quality:70,
+            destinationType: this.camera.DestinationType.DATA_URL,
+            sourceType: source,
+            allowEdit: true,
+            encodingType: this.camera.EncodingType.JPEG,
+            saveToPhotoAlbum: false,
+            correctOrientation: true
+          }
+          this.camera.getPicture(options).then(
+            (imgData) =>{
+              let base64Image = `data:image/jpeg; base64, ${imgData}`
+              callback(base64Image);
+            }, 
+            err => {
+              console.log('Problema ao capturar a foto', err);
+            });
+
+        } catch (error) {
+          console.log('problema ao tirar a foto', error);
+        }
+      });
+    }
+  }
+
+  public getPictureFromGalery(callback): void {
+    this._getPicture(this.camera.PictureSourceType.PHOTOLIBRARY, 
+      photo => { 
+        callback(photo) 
+      });
+  }
+
+  public takePicture(callback): void {
+    this._getPicture(this.camera.PictureSourceType.CAMERA,
+      photo => {
+        callback(photo)
+      });
   }
 
 }
